d4b521371cd7c870717f8d290f494856b0672ef5
travis <elendil7@protonmail.com>
2019-09-28 00:39:29 -0600

    moving snake around

--- snake.py
+++ snake.py
@@ -2,13 +2,17 @@ from os import system
 from pygame.locals import *
 import pygame
 import time
+import queue
+import copy
 
-width = 10 #width of grid
-height = 10 #height of grid
-x = width / 2 #current x position (start in middle)
-y = height / 2 #current y position (start in middle)
-direction = "right" #direction the snake is currently moving
+speed = 5 #speed the snake moves at
+width = 20 #width of grid
+height = 20 #height of grid
+x = int(width / 2) #current x position (start in middle)
+y = int(height / 2) #current y position (start in middle)
+direction = "right"
 map = None
+snek = queue.Queue()
 
 def snake():
 
@@ -19,6 +23,11 @@ def snake():
 	global y
 	global direction
 	global map
+	global snek
+	snek.put((int(x - 2), y))
+	snek.put((int(x - 1), y))
+	snek.put((x, y))
+
 	go = True
 
 	#loop to constantly listen for user input
@@ -28,26 +37,20 @@ def snake():
 		
 		if (keys[K_RIGHT]):
 			direction = "right"
-			x = x + 1
-			buildMap()
-		if (keys[K_LEFT]):
+		elif (keys[K_LEFT]):
 			direction = "left"
-			x = x - 1
-			buildMap()
-		if (keys[K_UP]):
+		elif (keys[K_UP]):
 			direction = "up"
-			y = y - 1
-			buildMap()
-		if (keys[K_DOWN]):
+		elif (keys[K_DOWN]):
 			direction = "down"
-			y = y + 1
-			buildMap()
-		if (keys[K_ESCAPE]):
+		elif (keys[K_ESCAPE]):
 			go = False
-		time.sleep (50.0 / 1000.0);
+
+		time.sleep (50.0 / (speed * 100.0));
+		slither()
+		printMap()
 
 def buildMap():
-	_ = system('clear')
 	temp = []
 
 	for i in range(height + 2):
@@ -57,23 +60,51 @@ def buildMap():
 				row.append("-")
 			elif(j == 0 or j == width + 1):
 				row.append("|")
-			elif(i == y and j == x):
-				row.append("X")
 			else:
 				row.append(" ")
 		temp.append(row)
 
 	global map
 	map = temp
-	printMap()
 
 def printMap():
+	_ = system('clear')
 	global map
 	for row in map:
 		full_row = ""
 		for r in row:
 			full_row = full_row + r + " "
 		print(full_row)
+
+def slither():
+	global x
+	global y
+	global direction
+	global snek
+	global map
+
+	# take away the tail and add to the head
+	tail = snek.get()
+	if(direction == "right"):
+		x = x + 1
+	elif(direction == "left"):
+		x = x - 1
+	elif(direction == "up"):
+		y = y - 1
+	elif(direction == "down"):
+		y = y + 1
+	snek.put((x,y))
+
+	# TODO if snake just ate an apple don't delete tail
+	map[tail[1]][tail[0]] = " " #remove tail from map
+
+	# TODO would be faster to just add new head to map b/c body is already there
+	
+	#add snake to map
+	for i in range(snek.qsize()):
+		t = snek.get()
+		map[t[1]][t[0]] = "X"
+		snek.put(t)
 
 
 if __name__ == "__main__":
